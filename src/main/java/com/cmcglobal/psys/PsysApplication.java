3ece6b3ba61d2a348965ac1307adc8338738392b
namlevu <namvule@hotmail.com>
2019-10-08 22:34:09 +0900

    init project

---
+++ src/main/java/com/cmcglobal/psys/PsysApplication.java
@@ -0,0 +1,13 @@
+package com.cmcglobal.psys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsysApplication.class, args);
+	}
+
+}
